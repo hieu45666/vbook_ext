750c0d846815381a4176e1b5a9e7657ea8dfaf08
hieu45666 <hieu2097@gmail.com>
2026-03-16 15:59:14 +0700

    Update Truyendocviet lan 3

--- Truyendocviet/src/search.js
+++ Truyendocviet/src/search.js
@@ -2,10 +2,14 @@ function execute(key, page) {
 
     if (!page) page = "1";
 
+    var host = "https://truyendocviet1.xyz";
+
     var url = page == "1"
-        ? "https://truyendocviet1.xyz/tim-kiem.html?q=" + key
-        : "https://truyendocviet1.xyz/tim-kiem/page-" + page + ".html?q=" + key;
+        ? host + "/tim-kiem.html?q=" + encodeURIComponent(key)
+        : host + "/tim-kiem/page-" + page + ".html?q=" + encodeURIComponent(key);
+
     console.log(url);
+
     var browser = Engine.newBrowser();
 
     browser.launch(url, 20000);
@@ -22,16 +26,42 @@ function execute(key, page) {
 
     for (var i = 0; i < items.size(); i++) {
 
-        var a = items.get(i).select(".product-info h3 a");
+        var item = items.get(i);
+
+        var a = item.select(".product-info h3 a");
+
+        if (!a || a.size() == 0) continue;
+
+        var name = a.text();
+        var link = host + a.attr("href");
+
+        var cover = "";
+        var img = item.select(".product-thumb img");
+        if (img && img.size() > 0) {
+            cover = img.attr("src");
+            if (cover.indexOf("http") != 0) {
+                cover = host + cover;
+            }
+        }
+
+        var description = "";
+        var author = item.select(".sg-rating a");
+        if (author && author.size() > 0) {
+            description = author.text();
+        }
 
         data.push({
-            name: a.text(),
-            url: "https://truyendocviet1.xyz" + a.attr("href")
+            name: name,
+            link: link,
+            cover: cover,
+            description: description,
+            host: host
         });
     }
 
     var next = (parseInt(page) + 1).toString();
 
-    if (data == null) return Response(data);
+    if (data.length == 0) return null;
+
     return Response.success(data, next);
 }
